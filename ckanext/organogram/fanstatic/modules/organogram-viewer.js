d1e5ff503c52e3d54b9683ee9d1c62e793b3c7f3
gregoxx <gsgibbs@me.com>
2021-03-31 13:12:52 +0100

    add license and copyright notices

--- ckanext/organogram/fanstatic/modules/organogram-viewer.js
+++ ckanext/organogram/fanstatic/modules/organogram-viewer.js
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2018 Keitaro AB
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 ckan.module('organogram-viewer', function ($, _) {
   function showError() {
     $('[data-module=organogram-viewer]').append('<div class="organogram-error"><span class="empty">' + _('Sorry, organogram data is currently unavailable.').fetch() + '</span></div>');
